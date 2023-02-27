2302adfb897d8ba82179331a26e8734e9c8b6a8b
Nakkealpha <93373647+Nakkealpha@users.noreply.github.com>
2023-02-27 13:44:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join @NAKFLIXTV to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join my channel first to use me 👇CLICK HERE 👇 \n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "POWERED BY @NAKFLIXTV")
